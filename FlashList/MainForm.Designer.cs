bbc88e9ce8d200fd004dd81d121380f2e88d3f5b
coolbas <coolbas87@yandex.ua>
2020-08-27 22:55:51 +0300

    Initial commit

---
+++ FlashList/MainForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿/*
+ * Сделано в SharpDevelop.
+ * Пользователь: ksf
+ * Дата: 15.09.2011
+ * Время: 12:17
+ * 
+ * Для изменения этого шаблона используйте Сервис | Настройка | Кодирование | Правка стандартных заголовков.
+ */
+namespace FlashList
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.lbFileList = new System.Windows.Forms.ListBox();
+            this.fbgDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bSaveNames = new System.Windows.Forms.Button();
+            this.bClose = new System.Windows.Forms.Button();
+            this.bSelectDirectory = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbFileList
+            // 
+            this.lbFileList.AllowDrop = true;
+            this.lbFileList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbFileList.Location = new System.Drawing.Point(0, 0);
+            this.lbFileList.Name = "lbFileList";
+            this.lbFileList.Size = new System.Drawing.Size(432, 360);
+            this.lbFileList.TabIndex = 0;
+            this.lbFileList.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListBox1DragDrop);
+            this.lbFileList.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListBox1DragEnter);
+            this.lbFileList.DragOver += new System.Windows.Forms.DragEventHandler(this.lbFileList_DragOver);
+            this.lbFileList.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListBox1MouseDown);
+            // 
+            // fbgDialog
+            // 
+            this.fbgDialog.Description = "Выберите папку:";
+            this.fbgDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.bSaveNames);
+            this.panel1.Controls.Add(this.bClose);
+            this.panel1.Controls.Add(this.bSelectDirectory);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 321);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(432, 39);
+            this.panel1.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Wingdings 3", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label5.Location = new System.Drawing.Point(281, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 15);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "\"";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(307, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "3.";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(150, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "2.";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Wingdings 3", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.label2.Location = new System.Drawing.Point(121, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "\"";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "1.";
+            // 
+            // bSaveNames
+            // 
+            this.bSaveNames.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bSaveNames.Enabled = false;
+            this.bSaveNames.Location = new System.Drawing.Point(186, 6);
+            this.bSaveNames.Name = "bSaveNames";
+            this.bSaveNames.Size = new System.Drawing.Size(75, 23);
+            this.bSaveNames.TabIndex = 11;
+            this.bSaveNames.Text = "Сохранить";
+            this.bSaveNames.UseVisualStyleBackColor = true;
+            // 
+            // bClose
+            // 
+            this.bClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bClose.Location = new System.Drawing.Point(343, 6);
+            this.bClose.Name = "bClose";
+            this.bClose.Size = new System.Drawing.Size(75, 23);
+            this.bClose.TabIndex = 10;
+            this.bClose.Text = "Выход";
+            this.bClose.UseVisualStyleBackColor = true;
+            this.bClose.Click += new System.EventHandler(this.bClose_Click);
+            // 
+            // bSelectDirectory
+            // 
+            this.bSelectDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bSelectDirectory.Location = new System.Drawing.Point(34, 6);
+            this.bSelectDirectory.Name = "bSelectDirectory";
+            this.bSelectDirectory.Size = new System.Drawing.Size(75, 23);
+            this.bSelectDirectory.TabIndex = 9;
+            this.bSelectDirectory.Text = "Обзор...";
+            this.bSelectDirectory.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 360);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbFileList);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ceed PlayList";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+		}
+		private System.Windows.Forms.ListBox lbFileList;
+		private System.Windows.Forms.FolderBrowserDialog fbgDialog;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bSaveNames;
+        private System.Windows.Forms.Button bClose;
+        private System.Windows.Forms.Button bSelectDirectory;
+    }
+}
---
+++ FlashList/MainForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using CarPlayList;
+
+namespace FlashList
+{
+	public partial class MainForm : Form
+	{
+        private List<FileItem> items = new List<FileItem>();
+        private BindingSource filesBinding = new BindingSource();
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		void ListBox1MouseDown(object sender, MouseEventArgs e)
+		{
+            if (lbFileList.SelectedItem != null)
+                lbFileList.DoDragDrop((/*FileInfo*/FileItem)lbFileList.SelectedItem, DragDropEffects.All);
+		}
+		
+		void ListBox1DragDrop(object sender, DragEventArgs e)
+		{
+            int index = lbFileList.IndexFromPoint(lbFileList.PointToClient(new Point(e.X, e.Y)));
+            FileItem drag_item = (FileItem)e.Data.GetData(typeof(FileItem));
+
+            if (index >= 0)
+            {
+                filesBinding.Remove(drag_item);
+                filesBinding.Insert(index, drag_item);
+            }
+        }
+
+        private void lbFileList_DragOver(object sender, DragEventArgs e)
+        {
+            lbFileList.SelectedIndex = lbFileList.IndexFromPoint(lbFileList.PointToClient(new Point(e.X, e.Y)));
+        }
+		
+		void ListBox1DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(typeof(FileItem))) 
+				e.Effect = DragDropEffects.Move;
+			else
+				e.Effect = DragDropEffects.None;
+		}
+
+        private void CreateFileList(string[] files)
+        {
+            items.Clear();
+            Array.Sort(files);
+
+            foreach (String fname in files)
+                items.Add(new FileItem { FileName = Path.GetFileName(fname), FullPath = fname });
+            filesBinding.ResetBindings(false);
+        }
+
+        private void bClose_Click(object sender, EventArgs e) => Application.Exit();
+
+        private void bSelectDirectory_Click(object sender, EventArgs e)
+        {
+            filesBinding.DataSource = items;
+            lbFileList.DataSource = filesBinding;
+            lbFileList.DisplayMember = "FileName";
+            lbFileList.ValueMember = "FileName";
+
+            if (fbgDialog.ShowDialog() == DialogResult.OK)
+            {
+                CreateFileList(Directory.GetFiles(fbgDialog.SelectedPath));
+                bSaveNames.Enabled = lbFileList.Items.Count > 0;
+            }
+        }
+
+        private void bSaveNames_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= items.Count - 1; i++)
+            {
+                FileItem item = items[i];
+
+                string OldPath = item.FullPath;
+                string Dir = Path.GetDirectoryName(OldPath);
+
+                item.FileName = item.FileName.TrimStart('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '.', ',', ' ', '_');
+                item.FileName = $"{i + 1:D2}_{item.FileName}";
+                item.FullPath = $"{Dir}{Path.DirectorySeparatorChar}{item.FileName}";
+
+                File.Move(OldPath, item.FullPath);
+            }
+            filesBinding.ResetBindings(false);
+        }
+    }
+}
